cc5775f87bb7a9b9ce48ea7ddafd90098e74f8a5
Helmut Granda <hgranda@us-aus-wireless-10-0-144-47.amer.corp.natinst.com>
2013-12-30 14:10:37 -0600

    Adding demo to page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+var note = document.getElementById("note");
+var getinfo = document.getElementById("getinfo");
+getinfo.onclick = function() {
+    console.log (note.value);
+    if (note.value == ""){
+        alert("you need input");
+    }else{
+        parsedata(note.value);
+    }
+}
+
+function parsedata(data){
+    var correctFormat = /NOTE OF(.*)>/.test(data);
+    if( !correctFormat){
+        alert("The string provided is incorrect");
+        return;
+    }
+    var longString = data.match("NOTE OF(.*)>");
+    var final = longString[1].replace(/:/g, "").replace(/-ON/g,"");
+    alert(final);  
+}
+
